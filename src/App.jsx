1b8e4b8599db374f80129d4545cb983a770bf156
Navin <navindena2432000@gmail.com>
2026-02-02 08:07:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>My React App</h1>
+      <p>This is a sample App.jsx file.</p>
+    </div>
+  );
+}
+
+export default App;
